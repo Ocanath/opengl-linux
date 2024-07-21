af35a5d8d88162e77723e92b223b1351b57a7a5c
ocanath <ocanath@gmail.com>
2024-07-20 22:19:40 -0700

    idk mars i guess

--- src/2.lighting/6.multiple_lights/multiple_lights.cpp
+++ src/2.lighting/6.multiple_lights/multiple_lights.cpp
@@ -133,19 +133,6 @@ int main()
         -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  0.0f,  0.0f,
         -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f,  1.0f
     };
-    // positions all containers
-    glm::vec3 cubePositions[] = {
-        glm::vec3( 0.0f,  0.0f,  0.0f),
-        glm::vec3( 2.0f,  5.0f, -15.0f),
-        glm::vec3(-1.5f, -2.2f, -2.5f),
-        glm::vec3(-3.8f, -2.0f, -12.3f),
-        glm::vec3( 2.4f, -0.4f, -3.5f),
-        glm::vec3(-1.7f,  3.0f, -7.5f),
-        glm::vec3( 1.3f, -2.0f, -2.5f),
-        glm::vec3( 1.5f,  2.0f, -2.5f),
-        glm::vec3( 1.5f,  0.2f, -1.5f),
-        glm::vec3(-1.3f,  1.0f, -1.5f)
-    };
     // positions of the point lights
     glm::vec3 pointLightPositions[] = {
         glm::vec3( 0.7f,  0.2f,  2.0f),
@@ -191,7 +178,8 @@ int main()
     lightingShader.setInt("material.diffuse", 0);
     lightingShader.setInt("material.specular", 1);
 
-	AssetModel ourModel("../../resources/objects/backpack/backpack.obj");
+	AssetModel backpack("../../resources/objects/backpack/backpack.obj");
+    AssetModel planet("../../resources/objects/planet/planet.obj");
     // render loop
     // -----------
     while (!glfwWindowShouldClose(window))
@@ -300,22 +288,20 @@ int main()
 
         // render containers
         glBindVertexArray(cubeVAO);
-        for (unsigned int i = 0; i < 10; i++)
-        {
-            // calculate the model matrix for each object and pass it to shader before drawing
-            glm::mat4 model = glm::mat4(1.0f);
-            model = glm::translate(model, cubePositions[i]);
-            float angle = 20.0f * i;
-            model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
-            lightingShader.setMat4("model", model);
 
-            glDrawArrays(GL_TRIANGLES, 0, 36);
-        }
-
-        mat4_t hw_backpack = Hz(currentFrame);        
+        mat4_t hw_backpack = Hz(0);        
+        hw_backpack.m[0][3] = 2.0;
+        hw_backpack.m[1][3] = 2.0;
+        hw_backpack.m[1][3] = -2.0;
         model = ht_matrix_to_mat4_t(hw_backpack);
         lightingShader.setMat4("model", model);
-        ourModel.Draw(lightingShader, NULL);
+        backpack.Draw(lightingShader, NULL);
+
+        hw_backpack.m[1][3] = -9.0;
+        model = ht_matrix_to_mat4_t(hw_backpack);
+        lightingShader.setMat4("model", model);
+        planet.Draw(lightingShader,NULL);
+
 
          // also draw the lamp object(s)
          lightCubeShader.use();
