2e1476ecd33b1e4a8d3adfcbbdf4b06f694b8ffb
ocanath <ocanath@gmail.com>
2025-10-10 13:32:09 -0700

    quicker builds

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <mujoco/mujoco.h>
 
+// MuJoCo data structures
+mjModel* m = NULL;                  // MuJoCo model
+mjData* d = NULL;                   // MuJoCo data
+
 
 int main(int argc, const char** argv)
 {
